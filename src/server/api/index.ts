fc49c149dd8d05d2d1bfd4d6fabac3623d4af59f
Amaro Mariño <amaromarinyo@landbot.io>
2022-10-30 22:21:03 +0100

    fix dependencies for server

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import data from "../data.json";
 
+const environment = process.env.NODE_ENV;
+console.log("ENVIRONMENT: ", environment);
 const app: Express = express();
 const port = process.env.PORT || 8080;
 const url = String(process.env.HOSTNAME).split("-");
